fe51ca7eb5eb674fa8fed42db1ec061e647ee35c
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-02-12 10:54:54 +0000

    update

---
+++ webapp/ext/PieChartAssets.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+        onSelectionChanged: function (oEvent) {
+            debugger;
+            var oSegment = oEvent.getParameter("segment");
+		}
+    };
+});
